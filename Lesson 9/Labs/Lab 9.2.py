ad0353572140745faebf3a846fff9cb0ff5bee2c
BetinNolazco <snolaza289@ops.org>
2015-12-04 10:27:49 -0600

    Updated Lab 9

--- Lesson 9/Labs/Lab 9.2.py
+++ Lesson 9/Labs/Lab 9.2.py
@@ -4,6 +4,67 @@
 
 import random
 
-compNum = random.randit(1, 21)
+compNum = random.randint(1, 21)
 
-user = int(input("Ask me a question: "))
+user = input("Ask me a question: ")
+userNum = int(input("Give me a number: "))
+
+if userNum == 1:
+    print ("It is certain")
+    
+elif userNum == 2:
+    print ("It is decidley so")
+
+elif userNum == 3:
+    print ("Without a doubt")
+
+elif userNum == 4:
+    print ("Yes, definitely")
+
+elif userNum == 5:
+    print ("You may relay on it")
+
+elif userNum == 6:
+    print ("A I see it, yes")
+
+elif userNum == 7:
+    print ("Most likely")
+    
+elif userNum == 8:
+    print ("Outlook good")
+
+elif userNum == 9:
+    print ("Yes")
+    
+elif userNum == 10:
+    print ("Signs porint to yes")
+    
+elif userNum == 11:
+    print ("Reply hazy try again")
+    
+elif userNum == 12:
+    print ("Ask agaim later")
+    
+elif userNum == 13:
+    print ("Better not tell you now")
+    
+elif userNum == 14:
+    print ("Cannot predict now")
+    
+elif userNum == 15:
+    print ("Concentrate and ask again")
+    
+elif userNum == 16:
+    print ("Don't count on it")
+elif userNum == 17:
+    print ("My reply is no")
+    
+elif userNum == 18:
+    print ("My soureces say no")
+    
+elif userNum == 19:
+    print ("Outlook not so good")
+    
+elif userNum == 20:
+    print ("Very doutful")
+    
